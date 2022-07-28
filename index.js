26dd2bf47cb700802e95251eae03c29fa35182b1
Carlos Neila <carlosneila@MBP-de-Carlos.home>
2022-07-28 14:32:15 +0200

    Prueba

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
+  // `path` input defined in action metadata file
   const nameToGreet = core.getInput('path');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(github.context.payload.after);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
