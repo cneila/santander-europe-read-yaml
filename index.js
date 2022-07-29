24c6449f0aad9a04821df6813ea38cd338fe4da6
Carlos Neila Ramos <109079582+cneila852338@users.noreply.github.com>
2022-07-29 11:57:59 +0200

    Prueba

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ try {
   outputfile = 'output.json',
   //yaml = require('js-yaml'),
   //fs = require('fs'),
-  obj = yaml1.load(fs.readFileSync(inputfile, {encoding: 'utf-8'}));
+  obj = yaml1.load(fs.readFileSync(`/home/runner/work/github-actions/github-actions/config.yaml`, {encoding: 'utf-8'}));
   // this code if you want to save
   fs.writeFileSync(outputfile, JSON.stringify(obj, null, 2));
 
