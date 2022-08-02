8582e6a1c9946973d87ed05b57c0f1100bf49221
Carlos Neila Ramos <46341067+cneila@users.noreply.github.com>
2022-08-02 14:45:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,23 +16,12 @@ try {
   console.log(github.context.payload.after);
   console.log(`The event payload: ${payload}`);
 
-
   
-  fs.readFile(`/home/runner/work/github-actions/github-actions/${nameToGreet}`, 'utf8',  (error, nameToGreet) => {
-    if (error) throw error;
-      console.log("El contenido es:" , nameToGreet);
-      var indentedJson = JSON.stringify(nameToGreet, null, 4);
-      console.log(`The event indentedJson: ${indentedJson}`);
-  });
-
-
   outputfile = 'output.json',
-  //yaml = require('js-yaml'),
-  //fs = require('fs'),
+    
   obj = yaml1.load(fs.readFileSync(`/home/runner/work/github-actions/github-actions/${nameToGreet}`, {encoding: 'utf-8'}));
-  // this code if you want to save
+  
   fs.writeFileSync(outputfile, JSON.stringify(obj, null, 2));
-
   
   fs.readFile(outputfile, 'utf8',  (error, content) => {
     if (error) throw error;
